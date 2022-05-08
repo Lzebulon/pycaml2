1bd4381fdc9220837f1a3c1ad72f13f11e934da6
Lzebulon <lzebulon@outlook.fr>
2022-05-08 17:18:52 +0200

    slow start

--- bin/main.ml
+++ bin/main.ml
@@ -2,21 +2,9 @@ let () =
   assert (Array.length Sys.argv > 1) ;
   let file = Sys.argv.(1) in print_endline file ;
   print_endline (Sys.getcwd ());
-  Parser.onTest;
 
   let ic = open_in file in
-  try 
-    let rec parse_file acc= match input_char ic with
-    | ' ' -> print_endline acc; if true then  parse_file "" else ()
-    | '\n' -> print_endline acc; parse_file ""
-    | ';' -> print_endline acc; parse_file ";"
-    | a -> parse_file (acc ^ (String.make 1 a) )
-    in
-    parse_file "";
-    flush stdout;
-    close_in ic  
-
-  with End_of_file -> print_endline "fini" 
-  | e -> close_in_noerr ic; raise e
+  let parsed = Parser.parse ic in
+  List.iter (Printf.printf "%s ") parsed;;
 
 
--- parsing/parser.ml
+++ parsing/parser.ml
@@ -1,1 +1,32 @@
-let onTest = print_endline "ouaaaaaa";;
+type valid_type = INT | STRING | FLOAT;;
+type arbre = Definition of arbre list * arbre | Variable of string * valid_type ;;
+
+
+
+(* let is_valid_token acc pile= match acc with
+| "let" -> Definition(recup_args, recup_corps)
+| _ -> pile;; *)
+
+let parse ic = 
+  let rec parse_file acc = try 
+      match input_char ic with
+      | ' ' -> print_endline acc; if true then  acc::(parse_file "") else []
+      | '\n' -> print_endline acc;print_endline "hop"; acc::(parse_file "")
+      | '\t' -> print_endline acc; print_endline "tabulation" ; acc::(parse_file "")
+      | ';' -> print_endline acc; acc::(parse_file ";") 
+      | '(' -> print_endline acc; acc::(parse_file "")
+      | ')' -> print_endline acc; acc::(parse_file "") 
+      | a -> parse_file (acc ^ (String.make 1 a) ) 
+    
+    with End_of_file -> flush stdout; close_in ic ;print_endline "fini"; [acc]
+    | e -> close_in_noerr ic; raise e
+  in
+    parse_file "" ;;
+
+let rec parcours_liste l = match l with
+| "let"::q -> let rec get_args q = match q with 
+                                  [] -> failwith "Il manque le =" 
+                                  |"="::_ -> [] 
+                                  | t::a -> Variable(t, STRING) :: (get_args a) in Definition(get_args q, Variable("a",STRING))
+| _::q-> parcours_liste q
+| [] -> Variable("a",FLOAT);;
