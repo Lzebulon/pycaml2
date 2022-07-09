dcbf7388fb62115792fddf63e0774a01d28e1518
Lzebulon <lzebulon@outlook.fr>
2022-07-09 16:24:45 +0200

    start of ast

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,7 @@ let () =
   assert (Array.length Sys.argv > 1) ;
   let file = Sys.argv.(1) in print_endline file ;
   print_endline (Sys.getcwd ());
-  Parser.lex_string (Parser.read_whole_file file);
-
+  let token_list =   Parser.lex_string (Parser.read_whole_file file) in
+  let ast = Parser.parse_token_list token_list in 
+  print_endline (Parser.string_of_ast ast);
 
--- parsing/parser.ml
+++ parsing/parser.ml
@@ -1,12 +1,13 @@
- type token_type = TOKEN_ID | TOKEN_EQUALS | TOKEN_LPAREN | TOKEN_RPAREN | TOKEN_LBRACE 
+ type token_type = TOKEN_ID | TOKEN_KEYWORD | TOKEN_EQUALS | TOKEN_LPAREN | TOKEN_RPAREN | TOKEN_LBRACE 
 | TOKEN_RBRACE | TOKEN_COLON | TOKEN_COMMA | TOKEN_LT | TOKEN_GT | TOKEN_INT | TOKEN_RIGHT_ARROW
-| TOKEN_SEMI | TOKEN_DOUBLE_COLON | TOKEN_EOF;;
+| TOKEN_SEMI | TOKEN_DOUBLE_COLON | TOKEN_EOF | NO_TOKEN;;
 
-type token_struct = {value :string; token_type : token_type;loc : int};;
+type debug = { mutable line : int; mutable column : int};;
 
-type lexer = {src : string; mutable actual_char : char ;src_size : int ;mutable loc : int; mutable line : int; mutable column : int} ;;
+type token_struct = {value :string; token_type : token_type;loc : int; debug : debug};;
+type lexer = {src : string; mutable actual_char : char ;src_size : int ;mutable loc : int; mutable debug : debug} ;;
 
-exception LexerError of {message: string; line : int; column : int};;
+exception LexerError of {message: string; debug : debug};;
 
 
 let init_token value tokenType= (value, tokenType) ;;
@@ -19,6 +20,7 @@ let read_whole_file filename =
 
 let string_of_token_type token = match token with
 | TOKEN_ID -> "TOKEN_ID"
+| TOKEN_KEYWORD -> "TOKEN_KEYWORD"
 | TOKEN_EQUALS -> "TOKEN_EQUALS"
 | TOKEN_LPAREN -> "TOKEN_LPAREN"
 | TOKEN_RPAREN -> "TOKEN_RPAREN"
@@ -32,22 +34,42 @@ let string_of_token_type token = match token with
 | TOKEN_RIGHT_ARROW-> "TOKEN_RIGHT_ARROW"
 | TOKEN_SEMI -> "TOKEN_SEMI"
 | TOKEN_DOUBLE_COLON -> "TOKEN_DOUBLE_COLON"
-| TOKEN_EOF-> "TOKEN_EOF";;
+| TOKEN_EOF-> "TOKEN_EOF"
+| NO_TOKEN -> "NO_TOKEN";;
 
-let string_of_token token = "< type = " ^ (string_of_token_type token.token_type) ^ " ; value = " ^ ( token.value) ^ " ; position approximative = " ^ (string_of_int token.loc) ^ " >";;
+let string_of_debug debug = "line "^string_of_int debug.line^", character "^string_of_int debug.column;;
+
+let string_of_token token = "< token_type = " ^ (string_of_token_type token.token_type) ^ " ; value = " ^ ( token.value) ^ " ; position approximative = " ^ (string_of_int token.loc) ^ 
+" ; debug = { "^string_of_debug token.debug^" }" ^" >";;
 
 let isalnum c = match c with '0'..'9'| 'a'..'z' | 'A'..'Z' | '_' -> true | _ -> false;;
-let isdigit c = match c with '0'..'9' -> true | _ -> false;;
+let isdigit c = match c with '0'..'9' | '_' -> true | _ -> false;;
 
+let keywords_list = ["and";"as";"assert"   ;   "asr"      ;   "begin"    ;   "class";
+"constraint" ; "do"      ;    "done"    ;    "downto"   ;   "else"    ;    "end";
+"exception"  ; "external"   ; "false"    ;   "for"      ;   "fun"       ;  "function";
+"functor"  ;   "if"       ;   "in"      ;    "include"  ;   "inherit" ;   " initializer";
+"land"     ;   "lazy "    ;   "let"      ;   "lor"      ;   "lsl"    ;     "lsr";
+"lxor"    ;   " match"   ;    "method"  ;    "mod"    ;     "module"   ;   "mutable";
+"new"      ;   "nonrec"    ;  "object"   ;   "of"   ;       "open"      ;  "or";
+"private"  ;   "rec"     ;    "sig      ;   struct"   ;   "then"   ;     "to";
+"true"     ;   "try"      ;   "type"    ;    "val"    ;     "virtual"    ; "when";
+"while"    ;   "with"];;
+
+let operator_list = ["+";"-";"*";"/"];;
+
+let iskeyword s = List.exists (fun x -> x = s) keywords_list;;
+
+let isoperator s = List.exists (fun x -> x = s) operator_list;;
 
 let lexer_advance lexer=
   if lexer.loc <= lexer.src_size then begin
     lexer.loc <- lexer.loc + 1 ;
-    lexer.column <- lexer.column + 1;
+    lexer.debug.column <- lexer.debug.column + 1;
     lexer.actual_char <- lexer.src.[lexer.loc];
     if lexer.actual_char = '\n' then
-      (lexer.line <- lexer.line + 1;
-      lexer.column <- 0; )
+      (lexer.debug.line <- lexer.debug.line + 1;
+      lexer.debug.column <- 0; )
   end;;
 
 let lexer_advance_with lexer token =
@@ -56,7 +78,7 @@ let lexer_advance_with lexer token =
 
 let lexer_advance_current lexer token_type =
   let value =Char.escaped lexer.actual_char in 
-  let tok = {value = value; token_type = token_type; loc = lexer.loc}
+  let tok = {value = value; token_type = token_type; loc = lexer.loc; debug = lexer.debug}
 in lexer_advance lexer;
 
 tok
@@ -71,8 +93,9 @@ let lexer_parse_id lexer =
     value := !value ^(Char.escaped lexer.actual_char);
     lexer_advance lexer;
   done;
-
-  {value = !value;token_type = TOKEN_ID; loc = lexer.loc} ;;
+  if iskeyword !value then {value = !value;token_type = TOKEN_KEYWORD; loc = lexer.loc; debug = lexer.debug}
+  else
+  {value = !value;token_type = TOKEN_ID; loc = lexer.loc; debug = lexer.debug} ;;
 
 let lexer_parse_number lexer =
     let value = ref "" in
@@ -81,7 +104,7 @@ let lexer_parse_number lexer =
       lexer_advance lexer;
     done;
   
-    {value = !value;token_type = TOKEN_INT; loc = lexer.loc};;
+    {value = !value;token_type = TOKEN_INT; loc = lexer.loc; debug = lexer.debug};;
 
 let lexer_skip_whitespace lexer =
   while match lexer.actual_char with ' '|'\t'| '\n' -> true | _ -> false do
@@ -98,7 +121,7 @@ let lexer_delete_comment lexer =
   with _ -> failwith "veuillez fermez les commentaires";;
 
 let rec lexer_next_token lexer =
-  if lexer.loc + 1<> lexer.src_size then begin
+  if lexer.loc + 1< lexer.src_size then begin
     lexer_skip_whitespace lexer;
     if isdigit lexer.actual_char then
       lexer_parse_number lexer
@@ -115,31 +138,77 @@ let rec lexer_next_token lexer =
     | ','-> lexer_advance_current lexer TOKEN_COMMA 
     | '<'-> lexer_advance_current lexer TOKEN_LT 
     | '>'-> lexer_advance_current lexer TOKEN_GT 
-    | '-' -> lexer_advance_current lexer TOKEN_RIGHT_ARROW
-    | ';'-> if lexer.src.[lexer.loc + 1] = ';' then lexer_advance_with lexer {value = ";;"; token_type = TOKEN_DOUBLE_COLON ; loc = lexer.loc}
-              else lexer_advance_with lexer {value = ";"; token_type =  TOKEN_COLON; loc = lexer.loc}
+    | '-' -> if lexer.src.[lexer.loc + 1] = '>' then lexer_advance_with lexer {value = "->"; token_type = TOKEN_RIGHT_ARROW ; loc = lexer.loc; debug = lexer.debug}
+    else lexer_advance_with lexer {value = "-"; token_type =  TOKEN_ID; loc = lexer.loc; debug = lexer.debug}
+    | '*' -> lexer_advance_with lexer {value = "*"; token_type =  TOKEN_ID; loc = lexer.loc; debug = lexer.debug}
+    | '+' -> lexer_advance_with lexer {value = "+"; token_type =  TOKEN_ID; loc = lexer.loc; debug = lexer.debug}
+    | ';'-> if lexer.src.[lexer.loc + 1] = ';' then (  lexer_advance_with lexer {value = ";;"; token_type = TOKEN_DOUBLE_COLON ; loc = lexer.loc; debug = lexer.debug})
+              else lexer_advance_with lexer {value = ";"; token_type =  TOKEN_COLON; loc = lexer.loc; debug = lexer.debug}
     
-    | _ -> raise (LexerError{message = "Unexpected Caracter : '"^Char.escaped lexer.actual_char^"'"; line = lexer.line; column = lexer.column}) end
+    | _ -> raise (LexerError{message = "Unexpected Caracter : '"^Char.escaped lexer.actual_char^"'"; debug = lexer.debug}) end
   
-  else {value = ""; token_type = TOKEN_EOF; loc = lexer.loc};;
+  else {value = ""; token_type = TOKEN_EOF; loc = lexer.loc; debug = lexer.debug};;
 
 
 let lex_string doc =
   let file = "filetest.pycaml" in
   try 
-    let lexer = {src = doc; actual_char = doc.[0] ; src_size = String.length doc ;loc = 0; line = 1; column = 1} in
-    let token = ref (lexer_next_token lexer) in
-    while (!token).token_type <> TOKEN_EOF do
-      print_endline (string_of_token !token);
-      token := lexer_next_token lexer
-    done;
-    print_endline (string_of_token !token);
+    let lexer = {src = doc; actual_char = doc.[0] ; src_size = String.length doc ;loc = 0; debug = {line = 1; column = 1}} in
+    let rec tokenize token = if (token).token_type <> TOKEN_EOF then begin
+      print_endline (string_of_token token);
+      token :: (tokenize (lexer_next_token lexer))
+    end else (print_endline (string_of_token token);[token]) in
+    tokenize (lexer_next_token lexer)
   with 
-    | LexerError e ->  print_endline ("File \""^file^"\", line "^string_of_int e.line^", character "^string_of_int e.column^": \n\t"^e.message) 
+    | LexerError e ->  failwith ("File \""^file^"\", line "^string_of_int e.debug.line^", character "^string_of_int e.debug.column^": \n\t"^e.message) 
   ;;
 
 
 (*Parser*)
+
+(*
+ident	::=	 (letter ∣ _) { letter ∣ 0…9 ∣ _ ∣ ' } 
+ 
+capitalized-ident	::=	 (A…Z) { letter ∣ 0…9 ∣ _ ∣ ' } 
+ 
+lowercase-ident	::=	(a…z ∣ _) { letter ∣ 0…9 ∣ _ ∣ ' } 
+ 
+letter	::=	 A…Z ∣ a…z
+   
+label-name	::=	 lowercase-ident
+*)
+
+(*
+infix-symbol	::=	(core-operator-char ∣ % ∣ <) { operator-char }
+
+operator-char	::=	~ ∣ ! ∣ ? ∣ core-operator-char ∣ % ∣ < ∣ : ∣ .
+
+core-operator-char	::=	$ ∣ & ∣ * ∣ + ∣ - ∣ / ∣ = ∣ > ∣ @ ∣ ^ ∣ |
+*)
+
+
+(*Keywords 
+   
+      and         "as"          "assert"      "asr"         begin       "class"
+      "constraint"  do          done        downto      else        end
+      exception   "external"    false       for         fun         function
+      "functor"     if          in          "include"     "inherit"    " initializer"
+      "land        lazy "       let         "lor         lsl         lsr"
+      "lxor"       " match"       method      mod         module      mutable
+      "new         nonrec      object      of          open        or"
+      "private"     rec         "sig         struct"      then        to
+      true        try         type        val         "virtual"     when
+      while       with
+
+
+    !=    #     &     &&    '     (     )     *     +     ,     -
+    -.    ->    .     ..    .~    :     ::    :=    :>    ;     ;;
+    <     <-    =     >     >]    >}    ?     [     [<    [>    [|
+    ]     _     `     {     {<    |     |]    ||    }     ~
+
+
+
+*)
 
 
 (*
@@ -156,6 +225,37 @@ float-literal	::=	[-] (0…9) { 0…9 ∣ _ } [. { 0…9 ∣
    constant ::= integer-literal | float-literal | string-literal | false | true | () | begin end
 *)
 
+(*Names
+   
+value-name	::=	lowercase-ident
+ 	            ∣	 ( operator-name )
+ 
+operator-name	::=	prefix-symbol ∣ infix-op
+ 
+infix-op	::=	infix-symbol
+ 	∣	 * ∣ + ∣ - ∣ -. ∣ = ∣ != ∣ < ∣ > ∣ or ∣ || ∣ & ∣ && ∣ :=
+ 	∣	 mod ∣ land ∣ lor ∣ lxor ∣ lsl ∣ lsr ∣ asr
+ 
+constr-name	::=	capitalized-ident
+ 
+tag-name	::=	capitalized-ident
+ 
+typeconstr-name	::=	lowercase-ident
+ 
+field-name	::=	lowercase-ident
+ 
+module-name	::=	capitalized-ident
+ 
+modtype-name	::=	ident
+ 
+class-name	::=	lowercase-ident
+ 
+inst-var-name	::=	lowercase-ident
+ 
+method-name	::=	lowercase-ident
+
+
+*)
 
 
 (*
@@ -183,4 +283,127 @@ expr	::=	value-path
 
 
 
-type ast = AST_SEQUENCE of ast list | AST_DECLARATION of string * ast list * ast list | CALL of string * string list | NONE ;;
+type ast = AST_SEQUENCE of ast list 
+          | AST_FUNCTION_DECLARATION of string * string list * ast * ast
+          | AST_CONSTANT_DECLARATION of string * ast * ast
+          | AST_CALL of string * ast list 
+          | AST_CONSTANT of {value : string; constant_type : token_type} 
+          |NONE ;;
+
+let rec string_list_to_string list = match list with
+| [] -> ""
+| t::[] -> " "^t
+| t::q -> " "^t^","^string_list_to_string q;;
+
+let rec string_of_ast_sequence list = match list with
+| [] -> ""
+| t::[] -> string_of_ast t^" ;"
+| t::q -> (string_of_ast t)^";\n"^string_of_ast_sequence q
+and string_of_ast_call list = match list with
+| [] -> ""
+| t::[] -> string_of_ast t^" "
+| t::q -> (string_of_ast t)^", "^string_of_ast_call q
+and 
+string_of_ast ast = match ast with
+| AST_FUNCTION_DECLARATION (a,b,c,d) ->  a^"("^string_list_to_string b^"){\n"^string_of_ast c ^"\n} in "^string_of_ast d
+| AST_CONSTANT_DECLARATION (a,c,d) ->  a^" = "^string_of_ast c ^" in "^string_of_ast d
+| AST_CONSTANT a -> a.value
+| AST_SEQUENCE a -> string_of_ast_sequence a
+| AST_CALL (a,b) -> a^"( "^string_of_ast_call b^" )"
+| NONE -> "AST_NONE";;
+
+
+type parser = {mutable token_list : token_struct list; mutable actual_token : token_struct};;
+
+let parser_get_next_token parser =
+  match parser.token_list with
+  | [] -> ()
+  | t::q -> parser.token_list <- q; 
+  parser.actual_token <- t;;
+
+let rec parse_patern parser =
+  parser_get_next_token parser;
+  if parser.actual_token.token_type = TOKEN_ID then let actualtok = parser.actual_token in actualtok.value :: parse_patern parser
+    else [];;
+
+let rec string_of_token_list token_list = match token_list with
+| [] -> ""
+| t::q -> string_of_token t ^ string_of_token_list q;;
+
+
+
+let rec parse_expr parser = 
+
+  let rec get_sequence_to list_stop parser= 
+    let rec aux () = 
+      parser_get_next_token parser;
+      if List.exists (fun x -> x = parser.actual_token.token_type) list_stop then 
+      [] else  (let actual_ast = parse_expr parser in actual_ast::aux())
+    in AST_SEQUENCE(aux ())
+
+  in
+
+  let rec get_argument parser = 
+    parser_get_next_token parser;
+    match parser.actual_token.token_type with
+    | TOKEN_ID -> let actual_token = parser.actual_token in AST_CALL(actual_token.value, [])::get_argument parser
+    | TOKEN_KEYWORD when parser.actual_token.value = "in" -> []
+    | TOKEN_COLON -> []
+    | TOKEN_DOUBLE_COLON -> []
+    | _ -> let actual_ast = parse_expr parser in actual_ast::get_argument parser
+
+
+  in
+
+  match parser.actual_token.token_type with  
+  | TOKEN_KEYWORD -> (match parser.actual_token.value with
+                | "let" -> begin 
+                  parser_get_next_token parser;
+                  let token_declaration = parser.actual_token in 
+                  let parameter = parse_patern parser in 
+                  if parser.actual_token.token_type = TOKEN_EQUALS then parser_get_next_token parser
+                  else failwith "add =";
+                  let expression = parse_expr parser in
+                  let inexpr = 
+                    parser_get_next_token parser ; 
+                    if parser.actual_token.value = "in" then parse_expr parser
+                    else NONE 
+                  in
+                  if parameter = [] then AST_CONSTANT_DECLARATION (token_declaration.value,expression,inexpr)
+                  else AST_FUNCTION_DECLARATION (token_declaration.value,parameter,expression,inexpr)
+                end
+                | "in" -> parser_get_next_token parser ; parse_expr parser
+                | _ -> NONE)
+  | TOKEN_ID -> (let call = parser.actual_token in AST_CALL(call.value,get_argument parser ))
+  | TOKEN_EQUALS -> NONE
+  | TOKEN_LPAREN -> get_sequence_to [TOKEN_LPAREN] parser
+  | TOKEN_RPAREN -> NONE
+  | TOKEN_LBRACE -> NONE
+  | TOKEN_RBRACE -> NONE
+  | TOKEN_COLON -> NONE
+  | TOKEN_COMMA -> NONE
+  | TOKEN_LT -> NONE
+  | TOKEN_GT -> NONE
+  | TOKEN_INT -> AST_CONSTANT {value = parser.actual_token.value; constant_type = parser.actual_token.token_type}
+  | TOKEN_RIGHT_ARROW -> NONE
+  | TOKEN_SEMI -> NONE
+  | TOKEN_DOUBLE_COLON -> NONE 
+  | TOKEN_EOF -> print_endline "call TOKEN_EOF"; NONE
+  | NO_TOKEN -> parser_get_next_token parser; parse_expr parser;;
+
+
+let parse_token_list token_list =
+  let parser = {token_list = token_list; actual_token = {value =""; token_type = NO_TOKEN;loc = 0; debug = {line = 0;column =0}}} in 
+  let rec parse_aux parser =
+    match parser.token_list with
+    | [] -> []
+    | [t] when t.token_type = TOKEN_EOF -> []
+    | _ -> let expression = parse_expr parser in parser_get_next_token parser; expression ::parse_aux parser
+  in AST_SEQUENCE(parse_aux parser);;
+
+
+
+
+
+
+
